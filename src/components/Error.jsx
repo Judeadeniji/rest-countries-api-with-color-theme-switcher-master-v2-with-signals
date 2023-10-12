83b9d3650816645dfe3cc48b0d0ed9226158298d
Olatoyan <golatoyan@gmail.com>
2023-10-12 01:46:03 +0100

    fixed 404 page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -3,7 +3,9 @@ import ErrorImg from "/error.svg";
 function Error() {
   return (
     <div className="flex justify-center flex-col items-center">
-      <h3 className="text-[3rem]">No country was found. Try again Later</h3>
+      <h3 className="text-[3rem] px-8 sma:text-[2rem] text-center">
+        No country was found. Try again Later
+      </h3>
       <img src={ErrorImg} alt="error page" className="w-[80rem]" />
     </div>
   );
